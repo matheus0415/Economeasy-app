6bae7098473fc372463fd7b46d992b72a35cd297
Matheus Santos <matheus.santos@fitbank.com.br>
2023-11-22 14:00:00 -0300

    Ajuste no diretorio e rotas

---
+++ Economeasy-client/src/main/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+// import { HashRouter } from 'react-router-dom'
+
+import Routes from './Routes'
+
+
+
+export default function App () {
+ 
+  return (
+        <div className="app">
+          <Routes />
+        </div>
+  )
+}
+
+
--- Economeasy-client/src/main/Routes.jsx
+++ Economeasy-client/src/main/Routes.jsx
@@ -1,14 +1,17 @@
-import React from "react";
 import { Navigate, Routes, Route } from 'react-router-dom'
 
 import Home  from '../components/home/Home'
 import ContactRegister from "../components/user/ContactRegister";
 import LoginPage from "../components/LoginPage/LoginPage";
 
-export default props =>
+export default function RoutesApp () {
+    return (
         <Routes>
             <Route path='/' element={<LoginPage/>} />
-            <Route path='/home' element={<Home/>} />
+            <Route path='home' element={<Home/>} />
             <Route path='/contact' element={<ContactRegister/>} />
             <Route path='*' element={<Navigate replace to='/' />} />
         </Routes>
+    )
+        
+} 
