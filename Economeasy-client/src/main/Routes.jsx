747d0150ab6ac8625c82c028a3f0f0f919cf484e
Matheus Soares <somathsant@gmail.com>
2023-11-27 00:02:04 -0300

    melhoria na pagina de registro

--- Economeasy-client/src/main/Routes.jsx
+++ Economeasy-client/src/main/Routes.jsx
@@ -1,15 +1,15 @@
 import { Navigate, Routes, Route } from 'react-router-dom'
 
 import Home  from '../components/home/Home'
-import ContactRegister from "../components/user/ContactRegister";
 import LoginPage from '../pages/loginPage/LoginPage'
+import RegisterPage from '../pages/registerPage/RegisterPage';
 
 export default function RoutesApp () {
     return (
         <Routes>
-            <Route path='/' element={<LoginPage/>} />
-            <Route path='home' element={<Home/>} />
-            <Route path='/contact' element={<ContactRegister/>} />
+            <Route path='/' element={<RegisterPage/>} />
+            <Route path='/loginPage' element={<LoginPage/>} />
+            <Route path='/registerPage' element={<RegisterPage/>} />
             <Route path='*' element={<Navigate replace to='/' />} />
         </Routes>
     )
---
+++ Economeasy-client/src/pages/registerPage/RegisterPage.jsx
@@ -0,0 +1,88 @@
+import { Formik } from 'formik'
+import React from 'react'
+import './style.css'
+import { Link } from 'react-router-dom'
+
+
+const RegisterPage = () => {
+
+    
+
+    return (
+        <div className='main'>
+
+            <div className='card-login'>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={values => {
+                        const errors = {};
+                        if (!values.email) {
+                            errors.email = 'Campo obrigatorio!';
+                        } else if (
+                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                        ) {
+                            errors.email = 'Email inválido!';
+                        }
+                        return errors;
+                    }}
+                    
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <div className='card'>
+                            <h1>Registrar</h1>
+                            <form onSubmit={handleSubmit}>
+                                <div className='textfield'>
+                                    <label >Email</label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.email}
+                                        placeholder='example@email.com'
+                                    />
+                                    {errors.email && touched.email && errors.email}
+
+                                </div>
+                                <div className='textfield'>
+                                <label >Senha</label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        value={values.password}
+                                        placeholder='********'
+                                    />
+                                    {errors.password && touched.password && errors.password}
+                                </div>
+                                <button className="btn-login" type="submit" disabled={isSubmitting}>
+                                    Cadastrar
+                                </button>
+                            </form>
+                            <p>Já possui uma conta? <Link to="/loginPage">Acesse aqui</Link></p>
+                        </div>
+                    )}
+                </Formik>
+
+            </div>
+        </div>
+
+    )
+}
+
+export default RegisterPage
